cfbd8050e2dbfbca0fdccdd73d36442101548b2e
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2024-09-25 16:29:51 +0200

    Fixes names

--- plugin/src/main/java/it/arsinfo/opennms/collector/b/plugin/AbstractServiceCollector.java
+++ plugin/src/main/java/it/arsinfo/opennms/collector/b/plugin/AbstractServiceCollector.java
@@ -47,7 +47,7 @@ public abstract class AbstractServiceCollector implements ServiceCollector {
         return
                 ImmutableCollectionSetResource.newBuilder(GenericTypeResource.class).setResource(
                                 ImmutableGenericTypeResource.newBuilder().setNodeResource(nodeResource)
-                                        .setType("CollectorB")
+                                        .setType("B")
                                         .setInstance("instance"+i)
                                         .build())
                         .addStringAttribute(createStringAttribute(group, "resourceName", "instance-name-"+i));
--- plugin/src/main/java/it/arsinfo/opennms/collector/b/plugin/ResourceTypesExtension.java
+++ plugin/src/main/java/it/arsinfo/opennms/collector/b/plugin/ResourceTypesExtension.java
@@ -2,22 +2,13 @@ package it.arsinfo.opennms.collector.b.plugin;
 
 import org.opennms.integration.api.v1.config.datacollection.ResourceType;
 import org.opennms.integration.api.xml.ClassPathResourceTypesLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class ResourceTypesExtension implements org.opennms.integration.api.v1.config.datacollection.ResourceTypesExtension {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ResourceTypesExtension.class);
-
     private final ClassPathResourceTypesLoader classPathResourceTypesLoader =
             new ClassPathResourceTypesLoader(ResourceTypesExtension.class, "collector-b-resource.xml");
-
-    public ResourceTypesExtension() {
-        for (ResourceType resourceType: classPathResourceTypesLoader.getResourceTypes())
-            LOG.warn(resourceType.getName());
-    }
 
     @Override
     public List<ResourceType> getResourceTypes() {
